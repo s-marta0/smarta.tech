33b3b9d77e1a51e8ff48ca506be4c383fa6ea9b6
sociophobic <torikowashi@gmail.com>
2021-02-23 09:13:40 +0300

    favicons, fonts, burger

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,10 +1,23 @@
 import React from "react"
+import ExternalLink from "../components/ExternalLink"
 
 
 class Contacts extends React.Component<{}, {}> {
   render = () =>
     <div className="Contacts">
-      Контактиксы: @the_sociophobic
+      <ExternalLink newTab to="https://t.me/ssmarta">
+        Telegram
+      </ExternalLink>, 
+      <ExternalLink newTab to="https://t.me/ssmarta">
+        VK
+      </ExternalLink>, 
+      <ExternalLink newTab to="https://www.instagram.com/slipper0">
+        Instagram
+      </ExternalLink><br /><br />
+      site by: <ExternalLink newTab to="https://t.me/the_sociophobic">
+        @the_sociophobic
+      </ExternalLink>
+
     </div>
 }
 
