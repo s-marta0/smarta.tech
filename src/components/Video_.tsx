7a471d3c5e20da33529c8ebffcd425d7f5088b9e
s-marta0 <nikitiser@gmail.com>
2021-03-23 14:48:52 +0300

    update

--- src/components/Video_.tsx
+++ src/components/Video_.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {isMobile} from 'react-device-detect';
 
 
 type Props = {
@@ -15,37 +16,37 @@ class Video extends React.Component<Props> {
   }
 
   render = () =>
-<div
-        className={`video ${this.props.className}`}
-      >
-        {this.props.autoplay ?
-          <iframe
-            src={`https://www.youtube.com/embed/${this.props.src}?autoplay=1&amp;loop=1&amp;enablejsapi=1&amp;&amp;playerapiid=featuredytplayer&amp;controls=0&amp;modestbranding=1&amp;rel=0&amp;showinfo=0&amp;color=white&amp;iv_load_policy=3&amp;theme=light&amp;wmode=transparent&amp;playlist=${this.props.src}&amp;mute=1`}
-            className={`video__iframe ${this.props.mobile && "desktop-only"} lazyload`}
-            title={this.props.src}
-            frameBorder="0"
-            allow="autoplay"
-            id="widget2"
-            loading="lazy"
-          /> 
-          :
-          <iframe
-            src={`https://www.youtube.com/embed/${this.props.src}`}
-            className={`video__iframe ${this.props.mobile && "desktop-only"} lazyload`}
-            title={this.props.src}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            loading="lazy"
-          />
-        }
-        <img
-          src={this.props.mobile}
-          className={`video__mobile ${this.props.mobile && "mobile-only"} intense lazyload`}
-          alt=""
+  <div
+    className={`video ${this.props.className}`}
+  >{isMobile && this.props.mobile != undefined ?    
+    <img
+      src={this.props.mobile}
+      className={`video__mobile ${this.props.mobile && "mobile-only"} intense lazyload`}
+      alt=""
+      loading="lazy"
+    />
+  :this.props.autoplay ?
+        <iframe
+          src={`https://www.youtube.com/embed/${this.props.src}?autoplay=1&amp;loop=1&amp;enablejsapi=1&amp;&amp;playerapiid=featuredytplayer&amp;controls=0&amp;modestbranding=1&amp;rel=0&amp;showinfo=0&amp;color=white&amp;iv_load_policy=3&amp;theme=light&amp;wmode=transparent&amp;playlist=${this.props.src}&amp;mute=1`}
+          className={`video__iframe ${this.props.mobile && "desktop-only"} lazyload`}
+          title={this.props.src}
+          frameBorder="0"
+          allow="autoplay"
+          id="widget2"
+          loading="lazy"
+        /> 
+        :
+        <iframe
+          src={`https://www.youtube.com/embed/${this.props.src}`}
+          className={`video__iframe ${this.props.mobile && "desktop-only"} lazyload`}
+          title={this.props.src}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
           loading="lazy"
         />
-      </div>
+      }
+ </div>
 }
 
 
