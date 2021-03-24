cb0411f071354c2f35851befc91adde758c1f63f
s-marta0 <martashilova@mail.ru>
2021-03-24 15:31:37 +0300

    update

--- src/data/graphic-design.ts
+++ src/data/graphic-design.ts
@@ -4,7 +4,18 @@ import { Project } from '../types'
 const data: Project[] = [
   {
     title: "Livre Géométrie fractale",
-    description: "En 2019 ma passion des fractales a commencé. J’ai composé un livre de 80 pages. J’ai lu beaucoup d’articles et composé ce livre à partir d’eux. En 2020 je l’ai amélioré et traduit en franÇais.",
+    description: "En 2019 ma passion des fractales a commencé. J’ai composé un livre de 80 pages. J’ai lu beaucoup d’articles et composé ce livre à partir d’eux. En 2020 je l’ai amélioré et traduit en français.",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
     blocks: [
       {
         link: "https://1.downloader.disk.yandex.ru/preview/5d3469983020016b6f15c5be701689fdadcb4e0debd088c23e0121cab3188768/inf/WSR0uZusXMy4BptleIrxUPmc1JUro3tAn2x_FixVZqPtT7_SmtYExNbNp73EHOkn8mbrxmlGoX4-t9WZOYRY3w%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-09%20%D0%B2%2019.36.25.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
@@ -14,141 +25,14 @@ const data: Project[] = [
     ],
   },
   {
-    title: "#BullRun2021 NFT Cryptoart collection",
-    description: "hiver 2021",
+    title: "Police Mantra",
+    description: "Développement cyrillique, création et animation d'affiche, 2019-2020",
     blocks: [
       {
-        link: "https://youtu.be/02tdnN6M6cI",
-        autoplay: true,
-      },
-      {
-        link: "https://youtu.be/L93qbj61zPg",
-        autoplay: true,
-      },
-      {
-        link: "https://youtu.be/5W2C_pPNZaQ",
-        autoplay: true,
-      },
-    ],
-  },
-  {
-    title: "Self isolation ⛓🖤 Introspection🕳",
-    description: "printemps 2020",
-    blocks: [
-      {
-        link: "https://www.youtube.com/watch?v=R-gpe9G6ZUI",
-        autoplay: true,
-      },
-      {
-        link: "https://www.youtube.com/watch?v=py5K3ZjmASU",
-        autoplay: true,
-      },
-      {
-        link: "https://www.youtube.com/watch?v=pkN7i85rV4g",
-        autoplay: true,
-      },
-    ],
-  },
-  {
-    title: "#Rassvet",
-    description: "logo creation & animation Digital Assets & Art Fund #Rassvet. hiver 2021",
-    blocks: [
-      {
-        link: " https://youtu.be/dmlUQXkQ5J4",
-        autoplay: false,
-      },
-
-    ],
-  },
-  {
-    title: "11.d",
-    description: "logo animation pour marque des vêtements",
-    blocks: [
-      {
-        link: "https://youtu.be/9Aa--gTAKeM",
-        autoplay: true,
-      },
-      {
-        link: "https://youtu.be/frf83HiRU_s",
-        autoplay: true,
-      },
-      {
-        link: "https://youtu.be/LARp-BHzr7c",
-        autoplay: true,
-      },
-    ]
-  },
-  {
-    title: "TrainFeel visualising",
-    description: "été 2020",
-    blocks: [
-      {
-        link: "https://www.youtube.com/watch?v=ljzm4yCfvs4",
+        link: "",
         autoplay: true,
       },
 
-    ],
-  },
-  {
-    title: "Mandelbrot fractal floating",
-    description: "été 2020",
-    blocks: [
-      {
-        link: "https://www.youtube.com/watch?v=L4HTShP16iI",
-        autoplay: true,
-      },
-
-    ],
-  },
-  {
-    title: "HumanDesign",
-    description: "automne 2019",
-    blocks: [
-      {
-        link: "https://www.youtube.com/watch?v=CjZSizTkXqk",
-        autoplay: true,
-      },
-      {
-        link: "https://www.youtube.com/watch?v=faow_zaMD4Y",
-        autoplay: true,
-      },
-      {
-        link: "https://www.youtube.com/watch?v=UisWdaaRkuM&t=3s",
-        autoplay: true,
-      },
-    ],
-  },
-  {
-    title: "Liquid chess création & animation",
-    description: "printemps 2019",
-    blocks: [
-      {
-        link: "https://www.youtube.com/watch?v=qyiGFyeaY_k",
-        autoplay: true,
-      },
-      {
-        link: "https://youtu.be/TMSguQKt4Ac",
-        autoplay: true,
-      },
-
-    ],
-  },
-  {
-    title: "Mantra font development, poster animation",
-    description: "printemps 2020",
-    blocks: [
-      {
-        link: "https://youtu.be/zdoDvcr7BtA",
-        autoplay: true,
-      },
-      {
-        link: "https://youtu.be/_dLRvB74Hxw",
-        autoplay: true,
-      },
-      {
-        link: "https://youtu.be/dcBBUG1VNxI",
-        autoplay: true,
-      },
     ],
   },
   {
@@ -156,11 +40,404 @@ const data: Project[] = [
     description: "",
     blocks: [
       {
-        link: "https://www.youtube.com/watch?v=kNV0_WZcso0",
+        link: "https://1.downloader.disk.yandex.ru/preview/f855f9bd9b2a3a4022721d13d67e6e6122cd82d5d4769908f576a75a9912d3d9/inf/FKMsxmjoZYPXFQuOOslnYC5-Bim0U3G_5ivPFeA4-ca7p6i5NHvEPGMXsNpCYCzhSd7rKS7_O85snxM3qSYvtg%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-22%20%D0%B2%2014.30.33.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2880x1642",
         autoplay: true,
       },
+
     ],
-  }
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://1.downloader.disk.yandex.ru/preview/a48392b5762e68da8a464e6220776022b3d743f3fa674ee0878436286a6475bd/inf/73UsL3jyiGgccKYijULlcfmc1JUro3tAn2x_FixVZqMPlGNCFHW17mvRFuxucSNak6ZlrwcYibvJjHWrLe75UA%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-22%20%D0%B2%2014.31.02.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2880x1642",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "Logo basé sur les initiales МШ",
+    description: "2017",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://4.downloader.disk.yandex.ru/preview/8bf5ca1055aac2ebb6071b6100bb3f8d7cfe62a385e3748fc12f6e03d57f05b5/inf/DGc8TZeSdGGRcWLoin47zPmc1JUro3tAn2x_FixVZqMdZXLmRYuuU-vbtAACftHhsIswNM5aw_YDP6jzmnQfug%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-22%20%D0%B2%2014.33.28.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "Logo Cardinalidae cardinalis",
+    description: "2017",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://1.downloader.disk.yandex.ru/preview/fa9b6920da843a1f311cd210490ecc3273a73507186072a939e2cd8d4afffc33/inf/kvR3tzR8zgvhfAbhmK8iTPmc1JUro3tAn2x_FixVZqP_EU5ppnH6TeJ2Y2tx78L5asvJh9xltCKz11H6IbLmMQ%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-22%20%D0%B2%2014.38.59.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://4.downloader.disk.yandex.ru/preview/7a7c1268a79cc80ec8d0af0c16c39479383570bf3aed08bdf495e8ceee6fb442/inf/6vnCjNt7HJ9L20xzNq5Bgi5-Bim0U3G_5ivPFeA4-cZO3gFkm9mh9vq99kPyHemcRJJFOk7JIlsUevip3BDLxw%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-22%20%D0%B2%2014.40.47.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "Autocollants pour une entreprise de voyages SPBCITYGROUP",
+    description: "2017",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://4.downloader.disk.yandex.ru/preview/c4b894e60e1a0c3e2e942909bd2e7114593067f2bf9308b38a7d825ec3d4fa23/inf/FsHADPHmwt2hNMyc4H6v-i5-Bim0U3G_5ivPFeA4-came15Dg8ZIgHYyvJiP2_pCHHtlmTg10K8tMZhVCH4-3Q%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-22%20%D0%B2%2014.47.51.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "Conception d'ensemble de pictogrammes pour l'instruction crypto-monnaie",
+    description: "2018",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://2.downloader.disk.yandex.ru/preview/de9fd69b3f51b9657fa598bfa1a65327eb919a054a981a7f44a0a0447640f0ba/inf/Y0iEJ4i-cL_aitbaHG_DbGeVyeA4ajZONS2SkAk5fCdsxhNJCrh0lW1RUxxCz6NFX4BHB0QNXr8hqo_NqnMkaw%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-22%20%D0%B2%2015.00.05.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://4.downloader.disk.yandex.ru/preview/4e14bef4b79935d9515fac98cad247b68dbe847c398295e0b925fdb0e1a00064/inf/_KwHOYHm-LGHXZu_m1rupS5-Bim0U3G_5ivPFeA4-caQNDYY-gg9DF4YRDcOKO7mwjCxWyUS-huu2u1sRHszbA%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-22%20%D0%B2%2015.04.10.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "Livre Rien de nouveau",
+    description: "2020",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://3.downloader.disk.yandex.ru/preview/326320ff156de0dd0d0596af4b9370ad99a6ea72cf7d08296e88bd686c04c31f/inf/D8OSNr4EOXuJFUrXDHF9pC5-Bim0U3G_5ivPFeA4-cYTCiht0n6cRSRSOI3Vh_TJUvMElc30Dqz_lWMDM-SeEA%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%200.15.24.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://4.downloader.disk.yandex.ru/preview/d6113ce9ec910308271f15cae31218708c55d5d11ba97d5a4008bd37911be064/inf/idpWkmWg7GqBZg3r7F5iLS5-Bim0U3G_5ivPFeA4-caPc4YZqrHKVP8yHWGYDY5zolQmkLeOnKyriYTEdVj7rg%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%200.26.53.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://3.downloader.disk.yandex.ru/preview/677309466583bfcb40f909a5ca00b2e51fa248bf3b10abc5a54873adc704d5e9/inf/wgW3_-smU2Hv71D9RcnfpoVzwuVCGK8E9IRKdTzg_WHmvwugmKMtXtyWoSXpKLjVvUsGwDyOZKiH4G-DD8GFWQ%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%200.27.49.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "Logo Aquanautics Aquadivers Alliance",
+    description: "2019",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://3.downloader.disk.yandex.ru/preview/4f12f3b0ae1a308d815bb3726fe3b19cead933fc470aaa1bfb11899fe32cb2e3/inf/73BnfYGj1YXB0pp0uR1GWYVzwuVCGK8E9IRKdTzg_WE5JGnQLCrzSmzzY4I_92GDYqM6QExTlWMEdII5cEJvRw%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%200.28.55.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "Logo Idigital",
+    description: "2019",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://3.downloader.disk.yandex.ru/preview/3d66ff99f46ccab2848c641b0cd87e9986e21a2ce98879c1fb8dd6c8a3b2406b/inf/EokrkKzd1eLBd4ANzHptXTVWCxfw_kQboO4OBCeJbyQSbWX2RS4Ib3eRftNwrqve_A2mkWRccrm7JSnSKcTQQA%3D%3D?uid=305797124&filename=digital.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },  
+  {
+    title: "Identité corporative du parc naturel Volcans du Kamtchatka",
+    description: "2021",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://1.downloader.disk.yandex.ru/preview/c3f3ed0d926dd50aa04f2f4c17a65902668c40ca470ee30dc8fc04c33e43d6d8/inf/-jawcspzAwjRYaz9WNw9j2eVyeA4ajZONS2SkAk5fCfgNeWJr3vlVnW5IFEhlXTjQQewxOPc_tjfIFCGnlo9zQ%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%2011.17.22.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  }, {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://1.downloader.disk.yandex.ru/preview/3beb61e6d96b0ef06bdffa5aa8e0c021e031ebf5c8dac0bd42cf36477f5aa21c/inf/oXObQsOZ3585FKr-1Sg08GeVyeA4ajZONS2SkAk5fCe4XRC8IVzL1YQhx7GUoLIr84jSso2z7qUYE7eOKgfgSg%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%2011.17.30.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  }, {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://1.downloader.disk.yandex.ru/preview/796df8843e9c61a8005a761e3901a61c1d80408e58ed2a510de8363516cbd275/inf/lztcX3nI_y_M15fIo0jq35U2WNxqY8enhaNZN04y2R1sHwLspoFFXm8ae_Xbr444kp3evEK7JMiBwG64jpGeeg%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%2011.21.02.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  }, {
+    title: "Identité visuelle de la boutique en ligne Hitbox",
+    description: "2020",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  }, {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://4.downloader.disk.yandex.ru/preview/ed578375f1b6796e4b4bfb18471109446c1cf8389c5467bb93e77b963b82422f/inf/bH9nCR12MiprKj0wEmg9p7S5e0S6RE0a3xcRlHu-H3fvK_Zaat6qEeDVZ023DVBvdSN0xg7nM1FxqlTCKP8l7A%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%2011.28.01.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  }, 
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://1.downloader.disk.yandex.ru/preview/9d433d5c4f09d031805428b174a79e5917813e6e78a2c9e81f0db8a999fd96d2/inf/Y0mKZ6db7Dnd0VqGqS86CWeVyeA4ajZONS2SkAk5fCf-scg_iZBpRm-Mpt7HQHvdGw6dzgi8-V_yOdZEERvb6g%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%2011.27.47.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  }, 
+  {
+    title: "Logo du festival de musique électronique Present perfect",
+    description: "2020",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
+  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://3.downloader.disk.yandex.ru/preview/ae464f0bd6f73113b426252f77bbe7e04d6b81e692c714cf460661ead3f46218/inf/oMigkbtw-4OD8rZWyoabUJU2WNxqY8enhaNZN04y2R3bkCuo7iC_wFiLZYWo0cEvnoMYuPOcDY0OEXDqlKqvcw%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%2011.31.00.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "Logo pour la fondation d'art numérique Rassvet",
+    description: "2021",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "https://3.downloader.disk.yandex.ru/preview/94d4d1b06b787735caf5d43225b57a2512a4014c90276994ed0357a0a745a8d7/inf/O4MquDBiGSFzpsFqPVE4FpU2WNxqY8enhaNZN04y2R14Y2uUh180TtRWgooYLufpgTg4QbS-EVBeZg6x_DgNyw%3D%3D?uid=305797124&filename=%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA%20%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0%202021-03-23%20%D0%B2%2011.29.59.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=305797124&tknv=v2&size=2158x1554",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },  {
+    title: "",
+    description: "",
+    blocks: [
+      {
+        link: "",
+        autoplay: true,
+      },
+
+    ],
+  },
 ]
 
 
